ed80fcc9e2bc599f05468a2c89c0205267075f90
Chip Carnes <chip.carnes27@gmail.com>
2017-02-02 11:55:07 -0600

    Add elevation stats to determine conditional markers

--- map1.py
+++ map1.py
@@ -3,19 +3,29 @@ import pandas
 
 # create pandas dataframe
 volcano_df = pandas.read_csv("Volcanoes-USA.txt")
+elev_col = volcano_df["ELEV"]
+lat_mean = volcano_df["LAT"].mean()
+long_mean = volcano_df["LON"].mean()
+
+# elevation data
+min_elev = min(elev_col)
+max_elev = max(elev_col)
+med_elev = (elev_col).median()
+low_quant = elev_col.quantile(0.25)
+high_quant = elev_col.quantile(0.55)
 
 # create the base map
-map = folium.Map(location=[45.372, -121.679],
+map = folium.Map(location=[lat_mean, long_mean],
                  zoom_start=4,
                  tiles='Stamen Terrain')
 
 
 def color_marker(elev):
-    if elev in range(0, 1000):
+    if elev in range(int(min_elev), int(low_quant)):
         color = 'green'
-    elif elev in range(1000, 2000):
+    elif elev in range(int(low_quant), int(med_elev)):
         color = 'orange'
-    elif elev in range(2000, 3000):
+    elif elev in range(int(med_elev), int(high_quant)):
         color = 'blue'
     else:
         color = 'red'
@@ -30,10 +40,11 @@ for lat, lon, name, elev in zip(volcano_df["LAT"],
     # map markers
     # make conditionally colored markers based on elevation
     # uses inline if statements -> may not satisfy PEP8
+    marker_label = name + str(elev) + "m"
     map.add_child(
         folium.Marker(
             [lat, lon],
-            popup=name,
+            popup=marker_label,
             icon=folium.Icon(
                 color=color_marker(elev)
             )
